a3ab11c48d806339a61574b34f323c6f7be213cf
Ivy Stephenson <istephensondev@gmail.com>
2021-03-17 12:48:13 -0500

    homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,11 +26,12 @@ router.get('/', (req, res) => {
         }
       ]
     })
-      .then(dbPostData => {
+    .then(dbPostData => {
         // pass a single post object into the homepage template
-        console.log(dbPostData[0]);
-        res.render('homepage', { posts: dbPostData[0].get({ plain: true }) });
-        
+        console.log({dbPostData})
+        console.log("dbPostData[0]", dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
       })
       .catch(err => {
         console.log(err);
